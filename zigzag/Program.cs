f3de7c87b34ec8e6a025b949c57b9f33cbcd275d
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-07 17:55:34 +0200

    added zigzag

---
+++ zigzag/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace zigzag
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var i = int.Parse(Console.ReadLine());
+            Console.WriteLine(i);
+        }
+    }
+}
