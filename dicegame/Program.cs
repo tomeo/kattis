f63a458419fb6d0de4151d072c25c2030b523b57
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-13 17:22:55 +0200

    dicegame

---
+++ dicegame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace dicegame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var gunnar = Console.ReadLine().Split(' ').Select(int.Parse).Sum();
+            var emma = Console.ReadLine().Split(' ').Select(int.Parse).Sum();
+            if (gunnar == emma) Console.WriteLine("Tie");
+            else if (gunnar > emma) Console.WriteLine("Gunnar");
+            else Console.WriteLine("Emma");
+        }
+    }
+}
