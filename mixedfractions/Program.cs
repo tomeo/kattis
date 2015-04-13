87cc21c435c254a5a5f6ae77fdb86af556fbe1c1
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-13 17:37:17 +0200

    mixedfractions

---
+++ mixedfractions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace mixedfractions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (numbers[0] == 0 || numbers[1] == 0) break;
+                Console.WriteLine(numbers[0] / numbers[1] + " " + numbers[0] % numbers[1] + " / " + numbers[1]);
+            }
+        }
+    }
+}
