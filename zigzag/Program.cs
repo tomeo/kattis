6a8c6f84fd8528989e9fd063088396521c429b10
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-13 17:08:54 +0200

    Almost

--- zigzag/Program.cs
+++ zigzag/Program.cs
@@ -7,12 +7,14 @@ namespace zigzag
     {
         public static void Main(string[] args)
         {
-            for (var i = 50; i <= 100; i++)
+            for (var i = 1000; i <= 2000; i++)
             {
                 var sw = new Stopwatch();
                 sw.Start();
-                var solution = new string(Solve(i));
-                Console.WriteLine("{0} = {1}", i, solution.ToLower());
+                var s = Solve(i);
+                Console.WriteLine("{0} = {1}", i, new string(s).ToLower());
+                var c = Calculate(s);
+                if (c != i) Console.WriteLine("ERROR: calculated to {0}", c);
                 sw.Stop();
                 //Console.WriteLine("Elapsed={0}", sw.Elapsed);
             }
@@ -27,13 +29,13 @@ namespace zigzag
             var r = v%25;
             var t = (l - 3) > 0 ? l - 3 : 0;
             var d = v - t * 25;
-            Console.WriteLine(
-                "v = {0} => l = {1}, q = {2}, t = {3}, d = {4}",
-                v,
-                CalculateMinLength(v),
-                q,
-                t,
-                d);
+            //Console.WriteLine(
+            //    "v = {0} => l = {1}, q = {2}, t = {3}, d = {4}",
+            //    v,
+            //    CalculateMinLength(v),
+            //    q,
+            //    t,
+            //    d);
             if (v <= 26) return new[] { (char)('A' + v - 1) };
             if (r == 0)
             {
@@ -45,11 +47,16 @@ namespace zigzag
             var x = d/2 + d%2;
             solution[0] = 'A';
             solution[1] = (char) ('A' + x);
+
+            for (var i = 2; i < solution.Length; i++)
+            {
+                solution[i] = (i%2 == 0) ? 'A' : 'Z';
+            }
+
             var y = d%2;
             if (y != 0) solution[solution.Length - 1] = (char) ('A' + y);
-            else solution[solution.Length - 1] = 'A';
 
-            Console.WriteLine("x = {0}, y = {1}", x, y);
+            //Console.WriteLine("x = {0}, y = {1}", x, y);
             
             return solution;
         }
@@ -78,6 +85,7 @@ namespace zigzag
 
         public static int Calculate(char[] carr)
         {
+            if (carr.Length == 1) return carr[0] - 64;
             var str = new string(carr);
             var sum = 0;
             for (var i = 0; i < str.Length - 1; i++)
