f0b2247a291935967f1eaa92d8461f85547d342a
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2015-04-13 22:16:31 +0200

    reversebinary

---
+++ reversebinary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace reversebinary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToInt32(new string(Convert.ToString(int.Parse(Console.ReadLine()), 2).ToCharArray().Reverse().ToArray()), 2));
+        }
+    }
+}
