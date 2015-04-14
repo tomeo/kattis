0e0d049b02c70f6a153bd5db47d1f23faf834f44
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-14 11:04:56 +0200

    solved

--- zigzag/Program.cs
+++ zigzag/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace zigzag
 {
@@ -7,36 +6,16 @@ namespace zigzag
     {
         public static void Main(string[] args)
         {
-            for (var i = 100; i <= 200; i++)
-            {
-                var sw = new Stopwatch();
-                sw.Start();
-                var s = Solve(i);
-                Console.WriteLine("{0} = {1}", i, new string(s).ToLower());
-                var c = Calculate(s);
-                if (c != i) Console.WriteLine("ERROR: should be {0}, but calculated to {1}", i, c);
-                sw.Stop();
-                //Console.WriteLine("Elapsed={0}", sw.Elapsed);
-            }
+            Console.WriteLine(new string(Solve(int.Parse(Console.ReadLine()))).ToLower());
         }
 
         public static char[] Solve(int v)
         {
-            
             var l = CalculateMinLength(v);
             var solution = new char[l];
-            var q = v/25;
             var r = v%25;
             var t = (l - 3) > 0 ? l - 3 : 0;
             var d = v - t * 25;
-            //Console.WriteLine(
-            //    "v = {0} => l = {1}, q = {2}, t = {3}, d = {4}",
-            //    v,
-            //    CalculateMinLength(v),
-            //    q,
-            //    t,
-            //    d);
-            if (v <= 26) return new[] { (char)('A' + v - 1) };
             if (r == 0)
             {
                 for (var i = 0; i < solution.Length; i++)
@@ -55,12 +34,8 @@ namespace zigzag
 
             var y = d%2;
             if (y != 0) solution[solution.Length - 1] = (char) ('A' + y);
-
-            //Console.WriteLine("v = {0} => x = {1}, y = {2}", v, x, y);
-
             var c = Calculate(solution);
             var diff = v - c;
-            //Console.WriteLine("Diff = {0}", diff);
             if (diff != 0)
             {
                 solution[solution.Length - 1] = (char) (solution[solution.Length - 1] + diff);
