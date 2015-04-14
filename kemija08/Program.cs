d3a306660cbb97d9738b04e17e88c740193cfa20
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-14 10:28:40 +0200

    kemija08

---
+++ kemija08/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace kemija08
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine()
+                .Replace("apa", "a")
+                .Replace("epe", "e")
+                .Replace("ipi", "i")
+                .Replace("opo", "o")
+                .Replace("upu", "u"));
+        }
+    }
+}
