4d6f20144d2015aa62ca066a0a646b660c768ea3
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-14 15:01:52 +0200

    easiest

---
+++ easiest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace easiest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (input == "0") return;
+                Console.WriteLine(Solve(int.Parse(input))); 
+            }            
+        }
+
+        private static int Solve(int input)
+        {
+            var current = 11;
+            var inputSum = Sum(input);
+            while (true)
+            {
+                if (inputSum == Sum(input*current))
+                {
+                    return current;
+                }
+                current++;
+            }
+        }
+
+        private static int Sum(int value)
+        {
+            return value.ToString().Sum(c => int.Parse(c.ToString()));
+        }
+    }
+}
