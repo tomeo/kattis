dc05accc76c816afef4ca688b266a5eda7affee4
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-14 16:38:41 +0200

    added billiard, unsolved

---
+++ billiard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace billiard
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (input == "0 0 0 0 0") return;
+                Console.WriteLine(Solve(input.Split(' ').Select(int.Parse).ToArray()));
+            }
+        }
+
+        private static string Solve(int[] input)
+        {
+            return string.Format("{0} {1}", input[0], input[1]);
+        }
+    }
+}
