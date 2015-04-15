3558f88fcbeca17453654590c49d7bc4aeb40634
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-15 16:15:48 +0200

    Removed redundancies

--- oddmanout/Program.cs
+++ oddmanout/Program.cs
@@ -8,15 +8,14 @@ namespace oddmanout
         public static void Main(string[] args)
         {
             Console.ReadLine();
-            string input;
             var n = 1;
-            while ((input = Console.ReadLine()) != null)
+            while (Console.ReadLine() != null)
             {
                 var codes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
                 Array.Sort(codes);
                 var i = 1;
                 while (i < codes.Length && codes[i] == codes[i - 1]) i += 2;
-                Console.WriteLine(string.Format("Case #{0}: {1}", n++, codes[i - 1]));
+                Console.WriteLine("Case #{0}: {1}", n++, codes[i - 1]);
             }
         }
     }
