a0219bcfb28cc1b51303a2f78ab64be44666a2a7
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-15 15:33:01 +0200

    engineeringenglish

---
+++ engineeringenglish/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace engineeringenglish
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var seen = new HashSet<string>();
+            var output = new List<List<string>>();
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                var line = new List<string>();
+                foreach (var word in input.Split(' '))
+                {
+                    var lc = word.ToLower();
+                    if (!seen.Contains(lc))
+                    {
+                        seen.Add(lc);
+                        line.Add(word);
+                    }
+                    else
+                    {
+                        line.Add(".");
+                    }
+                }
+                output.Add(line);
+            }
+            foreach (var str in output)
+            {
+                Console.WriteLine(string.Join(" ", str));
+            }
+        }
+    }
+}
