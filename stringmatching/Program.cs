35ca845c49ba12e1a2e423c7588b40f0a34c0b6a
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-16 11:34:01 +0200

    stringmatching time limit exceeded

---
+++ stringmatching/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stringmatching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern;
+            while ((pattern = Console.ReadLine()) != null)
+            {
+                var text = Console.ReadLine();
+                var indeces = new List<int>();
+                var index = 0;
+                while (index < text.Length)
+                {
+                    index = text.IndexOf(pattern, index, StringComparison.Ordinal);
+                    if (index == -1) break;
+                    indeces.Add(index);
+                    index++;
+                }
+                Console.WriteLine(string.Join(" ", indeces));
+            }
+        }
+    }
+}
