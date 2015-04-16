7e6d12820b858e5414a97e782c4deb94d01fcded
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-16 17:20:09 +0200

    started numbertree

---
+++ numbertree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace numbertree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var h = int.Parse(input[0]);
+            var root = (2 << h) - 1;
+            
+        }
+    }
+}
