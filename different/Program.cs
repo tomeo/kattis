7eda82f3da2053334267a79a0d56e0487454bb0e
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-16 12:07:21 +0200

    solved different and plantingtrees

--- different/Program.cs
+++ different/Program.cs
@@ -4,12 +4,16 @@ namespace different
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split(' ');
-            var d = Int64.Parse(input[0]) - Int64.Parse(input[1]);
-            if (d < 0) d *= -1;
-            Console.WriteLine(d);
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                var values = input.Split(' ');
+                var d = Int64.Parse(values[0]) - Int64.Parse(values[1]);
+                if (d < 0) d *= -1;
+                Console.WriteLine(d);
+            }
         }
     }
 }
---
+++ plantingtrees/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace plantingtrees
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var days = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(days, (x, y) => y.CompareTo(x));
+            for (var i = 0; i < days.Length; i++) days[i] += i + 1;
+            Console.WriteLine(days.Max()+1);
+        }
+    }
+}
