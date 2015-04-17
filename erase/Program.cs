a0d0bf72f9390f2469a315bb1780a58750bc532b
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-17 11:45:07 +0200

    erase wrong :(

---
+++ erase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace erase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var before = Convert.ToInt32(Console.ReadLine(), 2);
+            var after = Convert.ToInt32(Console.ReadLine(), 2);
+            Console.WriteLine(((n%2 != 0) ? (before & after) == 0 : before == after) ? "Deletion succeeded" : "Deletion failed");
+        }
+    }
+}
