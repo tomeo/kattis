89d17a032568360d91781a54ebc1d263a31a6c60
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2015-04-18 17:02:56 +0200

    anothercandies wrong answer :(

---
+++ anothercandies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace anothercandies
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cases = Int64.Parse(Console.ReadLine());
+            for (var i = 0; i < cases; i++)
+            {
+                Console.ReadLine();
+                var kids = Int64.Parse(Console.ReadLine());
+                var sum = 0L;
+                for (var j = 0; j < kids; j++)
+                {
+                    sum += Int64.Parse(Console.ReadLine());
+                }
+                Console.WriteLine((sum % kids == 0) ? "YES" : "NO");
+            }
+        }
+    }
+}
