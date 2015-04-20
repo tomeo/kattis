5578cf16b65a34da0b4929e1d35ae7abc083d9bf
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2015-04-21 00:23:07 +0200

    started looking at chemistvows

---
+++ chemistsvows/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chemistsvows
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var set = new HashSet<string>
+            {
+                {"h"},
+                {"he"},
+                {"li"},
+                {"be"},
+                {"b"},
+                {"c"},
+                {"n"},
+                {"o"},
+                {"f"},
+                {"ne"},
+                {"na"},
+                {"mg"},
+                {"al"},
+                {"si"},
+                {"p"},
+                {"s"},
+                {"cl"},
+                {"ar"},
+                {"k"},
+                {"ca"},
+                {"sc"},
+                {"ti"},
+                {"v"},
+                {"cr"},
+                {"mn"},
+                {"fe"},
+                {"co"},
+                {"ni"},
+                {"cu"},
+                {"zn"},
+                {"ga"},
+                {"ge"},
+                {"as"},
+                {"se"},
+                {"br"},
+                {"kr"},
+                {"rb"},
+                {"sr"},
+                {"y"},
+                {"zr"},
+                {"nb"},
+                {"mo"},
+                {"tc"},
+                {"ru"},
+                {"rh"},
+                {"pd"},
+                {"ag"},
+                {"cd"},
+                {"in"},
+                {"sn"},
+                {"sb"},
+                {"te"},
+                {"i"},
+                {"xe"},
+                {"cs"},
+                {"ba"},
+                {"hf"},
+                {"ta"},
+                {"w"},
+                {"re"},
+                {"os"},
+                {"ir"},
+                {"pt"},
+                {"au"},
+                {"hg"},
+                {"tl"},
+                {"pb"},
+                {"bi"},
+                {"po"},
+                {"at"},
+                {"rn"},
+                {"fr"},
+                {"ra"},
+                {"rf"},
+                {"db"},
+                {"sg"},
+                {"bh"},
+                {"hs"},
+                {"mt"},
+                {"ds"},
+                {"rg"},
+                {"cn"},
+                {"fl"},
+                {"lv"},
+                {"la"},
+                {"ce"},
+                {"pr"},
+                {"nd"},
+                {"pm"},
+                {"sm"},
+                {"eu"},
+                {"gd"},
+                {"tb"},
+                {"dy"},
+                {"ho"},
+                {"er"},
+                {"tm"},
+                {"yb"},
+                {"lu"},
+                {"ac"},
+                {"th"},
+                {"pa"},
+                {"u"},
+                {"np"},
+                {"pu"},
+                {"am"},
+                {"cm"},
+                {"bk"},
+                {"cf"},
+                {"es"},
+                {"fm"},
+                {"md"},
+                {"no"},
+                {"lr"},
+            };
+
+            Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(CanBeChemistried(input) ? "YES" : "NO");
+            }
+        }
+
+        private static bool CanBeChemistried(string input)
+        {
+            
+        }
+    }
+}
