70d924bd3061780eea1a9532c6d0f1384086910f
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2015-04-20 23:58:19 +0200

    solved quiteaproblem

---
+++ quiteaproblem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace quiteaproblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (input.ToLower().Contains("problem"))
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+        }
+    }
+}
