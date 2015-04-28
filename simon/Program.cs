b9e744118a19121eb1df40a97e4b81d665bc13f7
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-04-28 10:42:27 +0200

    solved simon

---
+++ simon/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace simon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (input.StartsWith("simon says "))
+                {
+                    Console.WriteLine(input.Replace("simon says ", string.Empty).Trim());
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
